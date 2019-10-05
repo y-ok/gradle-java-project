3282db6b9f4ee0449112882b5742693d0ec3ca52
y-ok <fairsky1985@gmail.com>
2019-10-06 03:57:01 +0900

    Google Formatterプラグイン追加

--- src/test/java/gradle/java/project/AppTest.java
+++ src/test/java/gradle/java/project/AppTest.java
@@ -3,12 +3,14 @@
  */
 package gradle.java.project;
 
-import org.junit.Test;
 import static org.junit.Assert.*;
 
+import org.junit.Test;
+
 public class AppTest {
-    @Test public void testAppHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull("app should have a greeting", classUnderTest.getGreeting());
-    }
+  @Test
+  public void testAppHasAGreeting() {
+    App classUnderTest = new App();
+    assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+  }
 }
